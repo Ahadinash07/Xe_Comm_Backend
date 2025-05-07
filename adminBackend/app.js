dd462af102697d382155fd2647f881b815e1f23c
Ahadinash07 <ahadinashhamza.gmail.com>
2025-05-07 12:49:55 +0530

    aws amplify

--- adminBackend/app.js
+++ adminBackend/app.js
@@ -20,7 +20,7 @@ const app = express();
 env.config();
 // app.use(cors());
 app.use(cors({
-    origin: "https://xecommfrontend-production.up.railway.app"
+    origin: "https://main.d39m47j0tu1pds.amplifyapp.com"
 }));
 app.use(express.json());
 
